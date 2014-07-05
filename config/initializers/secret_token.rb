c519030d394ec832b001cd4651005daad37bd20d
Charlie Massry <charlesdmassry@gmail.com>
2014-07-05 17:49:20 -0400

    created cucumber rspec rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showtime::Application.config.secret_token = 'e14a52f5bf206e022e13b847aa0f97fe7ce21784a7b2cea1e93301ebf23881e018812b3652d45235d76d3767a8a96668797fef7f4a3cd0c910581b1aa397ef4c'
